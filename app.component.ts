9622495aab636fc167b632ce0c750d46ee454d97
shaimaa22 <52531393+shaimaa22@users.noreply.github.com>
2023-11-15 16:40:56 +0200

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'task4';
+  Name =["mohamed" , "doaa" , "shimaa"];
+  Username =["hamada" , "dodao" , "shemo"];
+  index =["1" , "2" , "3"];
+  phone =["1364477697894" , "234155769867" , "34264768753225"];
+}
